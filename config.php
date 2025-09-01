10c56193bcf5dc0c411e47002c1987dab250c132
kasenovdilbek228-coder <kasenovdilbek228@gmail.com>
2025-09-02 01:39:59 +0500

    Update config.php
    Очистка файла нахуй

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
-define('VK_TOKEN', 'vk1.a.1KfG-kpQzbxG8hHHpC2hO14NDWY_NOii-7noW08zWsl9CRjQFA_TG5WeungyvIEQ1WCxYppjNEvcWMM2XmXgDu8ReEtAhCgcEKlrfqvvOaHGBCnMxWtmyOh8IqChIf_lMptR4mnf_xcCcyKK_M8BXXAN9w4kyPYMQ6LYK2jWSc21czUlqt9skRhQE5XbxCYhO-0Gyn2e8vr5wZTsl-zNXw');
+define('VK_TOKEN', 'Введите токен своего бота'''');
 define('GROUP_ID', 232159579); // ID вашей группы
 define('VK_API_VERSION', '5.199');
