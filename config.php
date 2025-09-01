9a4dbbecb50b6b43c39f374efcb600ff1a1e9bdb
kasenovdilbek228-coder <kasenovdilbek228@gmail.com>
2025-09-02 01:45:17 +0500

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
-define('VK_TOKEN', 'Введите токен своего бота'''');
+define('VK_TOKEN', 'Введите токен своего бота');
 define('GROUP_ID', 232159579); // ID вашей группы
 define('VK_API_VERSION', '5.199');
